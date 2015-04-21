dac93f65a0c942a6b9e37540c926e46a118e2c7a
Henrik Due <henrikduejensen@gmail.com>
2015-04-21 15:26:44 +0200

    Compare sql

--- Soem/Produktionsdata/01-ETL_Togproduktion_Fjern_og_Regional/compare_db.sql
+++ Soem/Produktionsdata/01-ETL_Togproduktion_Fjern_og_Regional/compare_db.sql
@@ -2,14 +2,18 @@
 
 :CONNECT oesmsqlt01\soem
 SELECT
-*
---count(*),sum(togkm)
---count(*),sum(antaltog) /*Broafgifter*/
+--*
+--count(*),sum(togkm),sum(togminutter)
+--count(*),sum(litrakm)
+count(*),sum(antaltog) /*Broafgifter*/
 --count(*),sum(antaltog_udenafgift),sum(antaltog_medafgift) /*Strkafgifter*/
 --count(*),sum(antal),sum(afgift_kr) /*Togproduktion_Afgifter*/ 
 --sum(togkm),sum(togtimer) /*Baneafgifter*/
 FROM  mdw_udv1.
 --[edw].[FT_Togproduktion_Afgifter]
+--[edw].[FT_Togproduktion_Litra]
+--[edw].[FT_Togproduktion_tog]
+[edw].[FT_Togproduktion_Togstandsninger]
 --[ods].[RDP_Strkafgifter]
 --[ods].[RDH_Strkafgifter]
 --[ods].[RD_Strkafgifter]
@@ -19,25 +23,30 @@ FROM  mdw_udv1.
 --[ods].[RDP_Baneafgifter]
 --[ods].[RD_Baneafgifter]
 --[ods].[RDH_Baneafgifter]
-[edw].[DI_Materiale]
+--[edw].[DI_Materiale]
 
 --where maaned='201503' /*Broafgifter Strkafgifter  */
 --where fk_di_tid='201503' and [status]='oprindelig'/*Togproduktion_Afgifter*/
+where fk_di_tid between 20150301 and 20150331-- and [status]='oprindelig'/*Togproduktion_Litra*/
 --group by AT_Togkategori
 --order by fk_di_togsystem,at_togkategori,at_afgiftsdriver
---with cube
+--with  cube
 go
 
 :CONNECT mssqlp01\alpha
 SELECT 
-*
---count(*),sum(togkm)
---count(*),sum(antaltog) /*Broafgifter*/
+--*
+--count(*),sum(togkm),sum(togminutter)
+--count(*),sum(litrakm)
+count(*),sum(antaltog) /*Broafgifter*/
 --count(*),sum(antaltog_udenafgift),sum(antaltog_medafgift) /*Strkafgifter*/
 --count(*),sum(antal),sum(afgift_kr) /*Togproduktion_Afgifter*/
 --sum(togkm),sum(togtimer) /*Baneafgifter*/
 FROM [mssqlp01\alpha].mdw.
 --[edw].[FT_Togproduktion_Afgifter] /*diff*/
+--[edw].[FT_Togproduktion_Litra]
+--[edw].[FT_Togproduktion_tog]
+[edw].[FT_Togproduktion_Togstandsninger]
 --[ods].[RDP_Strkafgifter]
 --[ods].[RDH_Strkafgifter]
 --[ods].[RD_Strkafgifter]
@@ -47,10 +56,11 @@ FROM [mssqlp01\alpha].mdw.
 --[ods].[RDP_Baneafgifter]
 --[ods].[RD_Baneafgifter]
 --[ods].[RDH_Baneafgifter]
-[edw].[DI_Materiale]
+--[edw].[DI_Materiale]
 
 --where maaned='201503' /*Broafgifter Strkafgifter  */
 --where fk_di_tid='201503' and [status]='oprindelig'/*Togproduktion_Afgifter*/
+where fk_di_tid between 20150301 and 20150331 --and [status]='oprindelig'/*Togproduktion_Litra*/
 --group by AT_Togkategori
 --order by fk_di_togsystem,at_togkategori,at_afgiftsdriver
 --with cube
