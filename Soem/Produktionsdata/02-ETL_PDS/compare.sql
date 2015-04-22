8f91ae7068cc7fb8d8fc1ae9a0ee7fce48b88cfc
Henrik Due <henrikduejensen@gmail.com>
2015-04-22 11:26:00 +0200

    Tilføjet pakke 120_edw_tr1_ft_OBSfravaer.dtsx

--- Soem/Produktionsdata/02-ETL_PDS/compare.sql
+++ Soem/Produktionsdata/02-ETL_PDS/compare.sql
@@ -1,19 +1,28 @@
 /*afvikles i SQLCMD mode. Query - SQLCMD*/
 
 :CONNECT oesmsqlt01\soem
-SELECT  *  
+SELECT  
+--*
+count(*)
+--count(*),sum(varighed_tim)  
 FROM  mdw_udv1.
-[edw].[FT_OBSOpgaver]where di_tid between 20150301 and 20150331
+--[edw].[FT_OBSOpgaver]
+--[edw].[FT_OBSArbejder]
+[edw].[FT_OBSFravaer]
+where di_tid between 20150301 and 20150331
 --with cube
 go
 
 :CONNECT mssqlp01\alpha
-SELECT *
+SELECT 
+--*
+count(*)
+--count(*),sum(varighed_tim)  
 FROM mdw.
-[edw].[FT_OBSOpgaver]
---where year([Tidsstempel])=2015 and month([Tidsstempel])=4
---where fk_di_tid='201502' -- and status='Oprindelig'
---group by AT_Togkategori
+--[edw].[FT_OBSOpgaver]
+--[edw].[FT_OBSArbejder]
+[edw].[FT_OBSFravaer]
+where di_tid between 20150301 and 20150331
 --with cube
 go
 
