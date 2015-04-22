4fa89c5a1aca2e0a39c511a56f1c73f52131f6f6
Henrik Due <henrikduejensen@gmail.com>
2015-04-22 12:33:45 +0200

    Tilføjet pakke 130_edw_tr2_ft_PDS.dtsx

--- Soem/Produktionsdata/02-ETL_PDS/compare.sql
+++ Soem/Produktionsdata/02-ETL_PDS/compare.sql
@@ -3,12 +3,14 @@
 :CONNECT oesmsqlt01\soem
 SELECT  
 --*
-count(*)
---count(*),sum(varighed_tim)  
+--count(*)
+--count(*),sum(varighed_tim) 
+count(*),sum(opgave_varighed_timer) 
 FROM  mdw_udv1.
 --[edw].[FT_OBSOpgaver]
 --[edw].[FT_OBSArbejder]
-[edw].[FT_OBSFravaer]
+--[edw].[FT_OBSFravaer]
+[edw].[FT_PDS]
 where di_tid between 20150301 and 20150331
 --with cube
 go
@@ -16,12 +18,14 @@ go
 :CONNECT mssqlp01\alpha
 SELECT 
 --*
-count(*)
---count(*),sum(varighed_tim)  
+--count(*)
+--count(*),sum(varighed_tim) 
+count(*),sum(opgave_varighed_timer)  
 FROM mdw.
 --[edw].[FT_OBSOpgaver]
 --[edw].[FT_OBSArbejder]
-[edw].[FT_OBSFravaer]
+--[edw].[FT_OBSFravaer]
+[edw].[FT_PDS]
 where di_tid between 20150301 and 20150331
 --with cube
 go
