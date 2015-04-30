adbbd9addd6ee7c63c3e3e1044016f89892a5738
HenrikDue <henrikduejensen@gmail.com>
2015-04-30 14:19:39 +0200

    Tilføjet pakke 135_EDW_TR3A_DI_S_Doegn_Inddeling.dtsx

--- Soem/Produktionsdata/006-ETL_Togproduktion_STOG/compare.sql
+++ Soem/Produktionsdata/006-ETL_Togproduktion_STOG/compare.sql
@@ -1,23 +1,32 @@
  /*
  ods.rd_stog_korelobtider OK
-
  ods.md_stog_stationer
-
  ods.rd_stog_Straekninger
-
+ edw.di_s_station_straekning
+ edw.DI_S_Materiel
+ ods.MD_Stog_litra_typer
+edw.DI_S_Straekning
+ ods.MD_Stog_finger_straekninger
+edw.DI_S_Doegn_Inddeling
 
 
  */
  /*afvikles i SQLCMD mode. Query - SQLCMD*/
 
 :CONNECT oesmsqlt01\soem
+--truncate table [MDW_udv1].edw.di_s_station_straekning 
+--go
 SELECT 
 --*
 count(*)  
 FROM  mdw_udv1.
 --ods.rd_stog_korelobtider
 --ods.md_stog_stationer
-ods.rd_stog_Straekninger
+--ods.rd_stog_Straekninger
+--edw.di_s_station_straekning
+--edw.DI_S_Materiel
+--ods.MD_Stog_finger_straekninger
+edw.DI_S_Straekning
 --with cube
 go
 
@@ -28,6 +37,10 @@ count(*)
 FROM mdw.
 --ods.rd_stog_korelobtider
 --ods.md_stog_stationer
-ods.rd_stog_Straekninger
+--ods.rd_stog_Straekninger
+--edw.di_s_station_straekning
+--edw.DI_S_Materiel
+--ods.MD_Stog_finger_straekninger
+edw.DI_S_Straekning
 --with cube
 go
