6ea958b41acd7381fbdcb99ad2dee19712fe6c8f
HenrikDue <henrikduejensen@gmail.com>
2015-05-04 10:31:23 +0200

    Tilføjet pakke 40_EDW_FT_Lokopersonale_S_tog.dtsx

--- Soem/Produktionsdata/007-ETL_PDS_STOG/compare.sql
+++ Soem/Produktionsdata/007-ETL_PDS_STOG/compare.sql
@@ -4,7 +4,9 @@ ods.TD_Strækning_Mapping
 [etl].[ods_TD_Strækning_Mapping_Standsninger]
 [ods].[TD_Strækning_S_Lokofører_Opgaver]
 edw.DI_S_Opgave_Straekning
-
+edw.DI_S_Opgave_Lokofører
+[edw].[FT_Lokopersonale_S_tog]
+edw.DI_Depot
 
  */
  /*afvikles i SQLCMD mode. Query - SQLCMD*/
@@ -20,7 +22,9 @@ FROM  mdw_udv1.
 --[ods].[RD_PDS_Lokofører_STOG]
 --[ods].[TD_Strækning_Mapping]
 --[ods].[TD_Strækning_S_Lokofører_Opgaver]
-edw.DI_S_Opgave_Straekning
+--edw.DI_S_Opgave_Straekning
+--edw.DI_S_Opgave_Lokofører
+[edw].[FT_Lokopersonale_S_tog]
 --where E_DAY_DATE BETWEEN DATEADD(day, -1, '2015-03-01' ) AND DATEADD(month, 1, '2015-03-01')
 --where [DI_Tid] between '20150301' and '20150331'
 --with cube
@@ -35,8 +39,10 @@ FROM mdw.
 --[ods].[RD_PDS_Lokofører_STOG]
 --[ods].[TD_Strækning_Mapping]
 --[ods].[TD_Strækning_S_Lokofører_Opgaver]
-edw.DI_S_Opgave_Straekning
+--edw.DI_S_Opgave_Straekning
+--edw.DI_S_Opgave_Lokofører
+[edw].[FT_Lokopersonale_S_tog]
 --where E_DAY_DATE BETWEEN DATEADD(day, -1, '2015-03-01' ) AND DATEADD(month, 1, '2015-03-01')
---where [DI_Tid] between '20150301' and '20150331'
+where [DI_Tid] between '20150301' and '20150331'
 --with cube
 go
