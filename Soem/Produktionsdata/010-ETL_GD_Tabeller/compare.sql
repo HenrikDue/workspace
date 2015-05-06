82dfc766236c125f688ed3176f386bf4d9c630a5
HenrikDue <henrikduejensen@gmail.com>
2015-05-06 10:36:58 +0200

    Tiilføjet pakke 040_GD_R_Lokofoerertid_STog.dtsx

--- Soem/Produktionsdata/010-ETL_GD_Tabeller/compare.sql
+++ Soem/Produktionsdata/010-ETL_GD_Tabeller/compare.sql
@@ -3,6 +3,7 @@ dbo.GD_R_Baneafgifter_FR
 dbo.GD_R_Togførertid_FR
 dbo.GD_R_Lokoførertid_FR
 dbo.GD_R_LønsumAnsættelsetype
+[dbo].[GD_R_Lokoførertid_STog]
 
  */
  /*afvikles i SQLCMD mode. Query - SQLCMD*/
@@ -17,7 +18,8 @@ FROM  mdw_udv1.
 --dbo.GD_R_Baneafgifter_FR
 --dbo.GD_R_Togførertid_FR
 --dbo.GD_R_Lokoførertid_FR
-dbo.GD_R_LønsumAnsættelsetype
+--dbo.GD_R_LønsumAnsættelsetype
+[dbo].[GD_R_Lokoførertid_STog]
 --where [DI_Tid] between '20150301' and '20150331'
 --with cube
 go
@@ -30,7 +32,8 @@ FROM poemaktuel.
 --dbo.GD_R_Baneafgifter_FR
 --dbo.GD_R_Togførertid_FR
 --dbo.GD_R_Lokoførertid_FR
-dbo.GD_R_LønsumAnsættelsetype
+--dbo.GD_R_LønsumAnsættelsetype
+[dbo].[GD_R_Lokoførertid_STog]
 where [PeriodeIndlæst]='201503'
 --where [DI_Tid] between '20150301' and '20150331'
 --with cube
