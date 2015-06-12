937eafe5a237a5e71658c9ead8905c51a9221103
HenrikDue <henrikduejensen@gmail.com>
2015-06-12 13:34:45 +0200

    Tilpasset ETL_LTD2 til batch kørsel

--- Soem/Produktionsdata/BatchFiler/Ltd2/10_Kør_LTD2.bat
+++ Soem/Produktionsdata/BatchFiler/Ltd2/10_Kør_LTD2.bat
@@ -5,10 +5,12 @@ SET DB_SERVER=Oesmsqlt01\soem
 SET DB_NAVN=MDW_UDV4
 SET SSISDB_FOLDER=%DB_NAVN%
 SET SOURCE_DRIVE=P:
-SET SOURCE_PATH=\70_BI\Projects\Files\FPC\
-SET SOURCE_FILE1=0642_POEM_FTE_leverance_
-SET DEST_PATH=\\%DB_SERVER%\files\%DB_NAVN%\HR_FTE\FPC\Excel\
-SET FILE_EXT=.xlsx
+SET SOURCE_PATH=\\gbpoemprod\Data\Aktuel\
+SET SOURCE_FILE1=lokofoerer_md_v2
+SET SOURCE_FILE2=lokofravaer_md_v2
+
+SET DEST_PATH=\\%DB_SERVER%\files\%DB_NAVN%\LTD\Data\Aktuel\
+SET FILE_EXT=.csv
 SET KOERSEL=test
 
 :STARTEN
@@ -16,7 +18,7 @@ CLS
 ECHO Script startet klokken: %time% 
 
 P:
-SET LOG_PATH=\70_BI\Data_load_kontrol\Prod\Load_step_04_HR_FTE
+SET LOG_PATH=\70_BI\Data_load_kontrol\Prod\Load_step_05_LTD\
 md %LOG_PATH%\Log
 
 setlocal enabledelayedexpansion
@@ -24,7 +26,7 @@ setlocal enabledelayedexpansion
 ECHO ******************************************************************************
 ECHO *
 SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = Value from ods.CTL_Dataload where kilde_system = 'Alle' and Variable = 'Master_periode'; print '*  Master LoadPeriode:  ---> '+@periode + ' <--- Tjek periode her.'" 
-SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,6) from ods.CTL_Dataload where kilde_system = 'FPC' and Variable = 'Last_Period_Load'; print '*  FPC HR_FTE LoadPeriode: '+@periode"
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,10) from ods.CTL_Dataload where kilde_system = 'LTD2' and Variable = 'Last_Period_Load'; print '*  LTD2 LoadPeriode: '+@periode"
 ECHO *
 ECHO ******************************************************************************
 ECHO.
@@ -35,14 +37,13 @@ IF %errorlevel%==3 GOTO ExitChosen
 IF %errorlevel%==2 SET ValgIndtastNyPeriode=1
 IF %errorlevel%==1 SET ValgMasterPeriode=1
 ECHO.
-REM if Valgindtastnyperiode == 0 så sæt masterperioden som load periode for protal (gøres ved at kalde exec [etl].[loadperiod_protal] uden værdi for perioden
 
-IF %ValgMasterPeriode%==1 SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "SET NOCOUNT ON;exec [etl].[loadperiod_fpc]" 
+IF %ValgMasterPeriode%==1 SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "SET NOCOUNT ON;exec [etl].[loadperiod_LTD2]" 
 IF %ValgMasterPeriode%==1 GOTO Valgslut
 
 :GenvalgPeriode
-IF  %ValgIndtastNyPeriode%==1 SET /p NYPERIODE="Indtast ny loadperiode yyyymm for FPC :"
-IF  %ValgIndtastNyPeriode%==1 SET QUERYTEXT=exec[etl].[loadperiod_fpc]'%NYPERIODE%01'
+IF  %ValgIndtastNyPeriode%==1 SET /p NYPERIODE="Indtast ny loadperiode yyyymm for LTD2 :"
+IF  %ValgIndtastNyPeriode%==1 SET QUERYTEXT=exec[etl].[loadperiod_LTD2]'%NYPERIODE%01'
 IF  %ValgIndtastNyPeriode%==1 (ECHO. & SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q %QUERYTEXT%)
 IF  %ValgIndtastNyPeriode%==1 CHOICE /C 123 /N /M "Tast 1: Start med valgt periode, 2: Indtast Ny dato eller 3: Fortryd og afslut"
 
@@ -50,11 +51,13 @@ IF %errorlevel%==3 GOTO ExitChosen
 IF %errorlevel%==2 SET ValgIndtastNyPeriode=1 & GOTO GenvalgPeriode
 
 :Valgslut
-ECHO 
-SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,6) from ods.CTL_Dataload where kilde_system = 'FPC' and Variable = 'Last_Period_Load'; print 'Starter med LoadPeriode: '+@periode"
-ECHO.
-for /f %%a in ('SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "SET NOCOUNT ON;select substring(Value,1,6) from ods.CTL_Dataload where kilde_system = 'FPC' and Variable = 'Last_Period_Load'" -h -1') do set PERIODE=%%a
 
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,10) from ods.CTL_Dataload where kilde_system = 'LTD2' and Variable = 'Last_Period_Load'; print 'Starter med LoadPeriode: '+@periode"
+ECHO.
+for /f %%a in ('SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "SET NOCOUNT ON;select substring(Value,1,10) from ods.CTL_Dataload where kilde_system = 'LTD2' and Variable = 'Last_Period_Load'" -h -1') do set PERIODE=%%a
+SET tmp1=%PERIODE:~6,4%
+SET tmp2=%PERIODE:~3,2%
+SET PERIODE=%tmp1%%tmp2%
 SET LOGFILE=LOG\Log_%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_%KOERSEL%.txt
 SET LOGFILE=%LOGFILE: =0%
 
@@ -63,8 +66,9 @@ ECHO Folder:  %cd%  >> %LOGFILE%
 pause
 
 ECHO f | xcopy /y %SOURCE_PATH%%SOURCE_FILE1%%PERIODE%%FILE_EXT% %DEST_PATH%%SOURCE_FILE1%%PERIODE%%FILE_EXT%
+ECHO f | xcopy /y %SOURCE_PATH%%SOURCE_FILE2%%PERIODE%%FILE_EXT% %DEST_PATH%%SOURCE_FILE2%%PERIODE%%FILE_EXT%
 
-SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "exec etl.run_etl_HR_FTE %SSISDB_FOLDER%, ''" >> %LOGFILE%
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "exec etl.run_etl_LTD2 %SSISDB_FOLDER%, ''" >> %LOGFILE%
 ECHO ******************************************************************************
 ECHO.
 %LOGFILE%
