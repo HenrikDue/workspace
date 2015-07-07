49f05d7d1d3ba4f81fd3fc9e561db946c5ae993e
HenrikDue <henrikduejensen@gmail.com>
2015-07-07 10:04:12 +0200

    Ændret servere til DWMART og APS databaser

---
+++ Soem/Produktionsdata/BatchFiler/PdsSTog/10_Kør_PDS_S_Tog_Import.bat
@@ -0,0 +1,70 @@
+ECHO OFF
+SET ValgIndtastNyPeriode=0
+SET ValgMasterPeriode=0
+SET DB_SERVER=Oesmsqlt01\soem
+SET DB_NAVN=MDW_UDV4
+SET SSISDB_FOLDER=%DB_NAVN%
+
+SET DEST_PATH=\\%DB_SERVER%\files\%DB_NAVN%\PDS_STOG\
+SET KOERSEL=test
+
+:STARTEN
+CLS
+ECHO Script startet klokken: %time% 
+
+P:
+SET LOG_PATH=\70_BI\Data_load_kontrol\Load_step_09_PDS_S-tog
+md %LOG_PATH%\Log
+
+setlocal enabledelayedexpansion
+
+ECHO ******************************************************************************
+ECHO *
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = Value from ods.CTL_Dataload where kilde_system = 'Alle' and Variable = 'Master_periode'; print '*  Master LoadPeriode:  ---> '+@periode + ' <--- Tjek periode her.'" 
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,6) from ods.CTL_Dataload where kilde_system = 'S-tog PDS' and Variable = 'Load_Period'; print '*  S-tog PDS LoadPeriode: '+@periode"
+ECHO *
+ECHO ******************************************************************************
+ECHO.
+
+CHOICE /C 123 /N /M "Tast 1: Start med master loadperiode, 2: Indtast Ny dato eller 3: Fortryd og afslut"
+
+IF %errorlevel%==3 GOTO ExitChosen
+IF %errorlevel%==2 SET ValgIndtastNyPeriode=1
+IF %errorlevel%==1 SET ValgMasterPeriode=1
+ECHO.
+
+IF %ValgMasterPeriode%==1 SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "exec [etl].[loadperiod_s_tog_PDS]" 
+IF %ValgMasterPeriode%==1 GOTO Valgslut
+
+:GenvalgPeriode
+IF  %ValgIndtastNyPeriode%==1 SET /p NYPERIODE="Indtast ny loadperiode yyyymm for S-tog:"
+IF  %ValgIndtastNyPeriode%==1 SET QUERYTEXT=exec[etl].[loadperiod_s_tog_PDS]'%NYPERIODE%01'
+IF  %ValgIndtastNyPeriode%==1 (ECHO. & SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q %QUERYTEXT%)
+IF  %ValgIndtastNyPeriode%==1 CHOICE /C 123 /N /M "Tast 1: Start med valgt periode, 2: Indtast Ny dato eller 3: Afslut"
+
+IF %errorlevel%==3 GOTO ExitChosen
+IF %errorlevel%==2 SET ValgIndtastNyPeriode=1 & GOTO GenvalgPeriode
+
+:Valgslut
+
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "declare @periode varchar(50); select @periode = substring(value,1,6) from ods.CTL_Dataload where kilde_system = 'S-tog PDS' and Variable = 'Load_Period'; print 'Starter med LoadPeriode: '+@periode"
+ECHO.
+
+SET LOGFILE=LOG\Log_%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%_%KOERSEL%.txt
+SET LOGFILE=%LOGFILE: =0%
+
+cd %LOG_PATH%
+ECHO Folder:  %cd%  >> %LOGFILE%
+pause
+
+SQLCMD -S %DB_SERVER% -d %DB_NAVN% -E -Q "exec etl.run_etl_PDS_STOG %SSISDB_FOLDER%, ''" >> %LOGFILE%
+ECHO ******************************************************************************
+ECHO.
+%LOGFILE%
+pause
+
+%DEST_PATH%Log\logDiff.txt
+%DEST_PATH%Log\logDiffDet.txt
+
+:ExitChosen
+
